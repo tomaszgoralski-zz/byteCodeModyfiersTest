39ee05a2fa683f74315034c8a9dcc8d720386f43
Tomasz Góralski <tomasz_goralski@wp.pl>
2014-12-29 20:30:39 +0100

    init commit

---
+++ CGLIBTestProject/src/main/java/reflection/Main.java
@@ -0,0 +1,15 @@
+package reflection;
+
+public class Main {
+
+	@SuppressWarnings("unused")
+	private final static String MY_STATIC = "myStatic";
+
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+
+}
