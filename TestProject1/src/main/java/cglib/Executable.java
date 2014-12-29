f8012497966fceaf468174ed5e856dbef0c5c640
Tomasz Góralski <tomasz_goralski@wp.pl>
2014-12-29 21:14:59 +0100

    move project

---
+++ TestProject1/src/main/java/cglib/Executable.java
@@ -0,0 +1,10 @@
+package cglib;
+
+public class Executable {
+
+	public void executeMe() {
+		System.out.println("execution begins");
+		// real time operation here
+		System.out.println("execution ends");
+	}
+}
